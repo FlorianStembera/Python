bb6300dd28a0d57e1761ed03769d52a2d39a0e47
stemberp <pstembera@gmail.com>
2022-10-20 00:23:50 +0200

    - zmena zadani hodnot - refactoring

--- vypocet_objemu_obsahu_sestibokeho_jehlanu.py
+++ vypocet_objemu_obsahu_sestibokeho_jehlanu.py
@@ -1,29 +1,35 @@
-#kalkulacka - objem a obsah hexapyramid
+'''
+    @Florian Stembera
+    - vypocet objemu a obsahu hexajehlanu    
+'''
 
 import math
 
-h = float(input('vyska telesa? '))
-
-while  h <= 0:
-    print('Vyska telesa nesmi byt mensi nebo rovna nule!')
+#nacitani vysky telesa
+while True:
     h = float(input('výška objektu? '))
+    if h<=0:
+        print('Vyska telesa nesmi byt mensi nebo rovna nule!')
+    else:
+        break
 
-s = float(input('delka strany telesa? '))
-
-while s <= 0:
-    print('Delka strany telesa nesmi byt mensi nebo rovna nule!')
+#nacitani delky strany telesa
+while True:
     s = float(input('délka strany? '))
+    if s<=0:
+        print('Delka strany telesa nesmi byt mensi nebo rovna nule!')
+    else:
+        break
 
-#objem objektu
-
+#vypocet objemu telesa
 V = (math.sqrt(3) / 2) * s ** 2 * h
 
-#obsah objektu
-
+#vypocet obsahu telesa
 P = (3 * math.sqrt(3) / 2) * (s ** 2)
 N = ((3 * s) * math.sqrt((h ** 2) + (3 * (s ** 2) / 4)))
 S = P + N
 
+#zobrazeni vyslednych hodnot
 print('objem objektu je %.2f' % V)
 print('obsah objektu je %.2f' % S)
 
